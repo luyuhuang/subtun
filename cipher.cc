94943e5acbb10092ecaa0b4070414da75b3685b4
luyuhuang <luyu_huang@foxmail.com>
2022-09-14 18:33:34 +0800

    add aes-128-gcm

--- cipher.cc
+++ cipher.cc
@@ -146,3 +146,90 @@ error:
 	ERR_error_string_n(err, buf, sizeof(buf));
 	throw runtime_error(buf);
 }
+
+size_t aes_128_gcm::encrypt(const key_type &key, const uint8_t *data, size_t len, uint8_t *encrypted, size_t cap) {
+	thread_local static EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+	if (cap < len + min_cap) throw runtime_error("cap is not large enough");
+
+	int n;
+	size_t ans = 0;
+
+	if (!ctx)
+		goto error;
+	if (!EVP_EncryptInit_ex2(ctx, EVP_aes_128_gcm(), nullptr, nullptr, nullptr))
+		goto error;
+	if (auto k = EVP_CIPHER_CTX_get_key_length(ctx); k != key.size())
+		throw runtime_error("key length is wrong. expect " + to_string(k) + " got " + to_string(key.size()));
+	if (EVP_CIPHER_CTX_get_iv_length(ctx) != iv_size)
+		throw logic_error("iv length is wrong!");
+
+	RAND_bytes(encrypted, iv_size);
+	if (!EVP_EncryptInit_ex2(ctx, nullptr, key.c_str(), encrypted, nullptr))
+		goto error;
+
+	encrypted += iv_size;
+	ans += iv_size;
+
+	if (!EVP_EncryptUpdate(ctx, encrypted, &n, data, len))
+		goto error;
+
+	encrypted += n;
+	ans += n;
+
+	if (!EVP_EncryptFinal(ctx, encrypted, &n))
+		goto error;
+
+	encrypted += n;
+	ans += n;
+
+	if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, tag_size, encrypted))
+		goto error;
+
+	return ans + tag_size;
+
+error:
+	auto err = ERR_get_error();
+	thread_local static char buf[256];
+	ERR_error_string_n(err, buf, sizeof(buf));
+	throw runtime_error(buf);
+}
+
+size_t aes_128_gcm::decrypt(const key_type& key, const uint8_t* data, size_t len, uint8_t* decrypted, size_t cap) {
+	thread_local static EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+	thread_local static uint8_t tag[tag_size];
+
+	int n0, n1;
+
+	if (!ctx)
+		goto error;
+	if (!EVP_DecryptInit_ex2(ctx, EVP_aes_128_gcm(), nullptr, nullptr, nullptr))
+		goto error;
+	if (auto k = EVP_CIPHER_CTX_get_key_length(ctx); k != key.size())
+		throw runtime_error("key length is wrong. expect " + to_string(k) + " got " + to_string(key.size()));
+	if (EVP_CIPHER_CTX_get_iv_length(ctx) != iv_size)
+		throw logic_error("iv length is wrong!");
+
+	if (!EVP_EncryptInit_ex2(ctx, nullptr, key.c_str(), data, nullptr))
+		goto error;
+
+	data += iv_size;
+	len -= iv_size + tag_size;
+
+	if (!EVP_DecryptUpdate(ctx, decrypted, &n0, data, len))
+		goto error;
+
+	memcpy(tag, data + len, tag_size);
+	if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, tag_size, tag))
+		goto error;
+
+	if (EVP_DecryptFinal(ctx, decrypted + n0, &n1))
+		goto error;
+
+	return n0 + n1;
+error:
+	auto err = ERR_get_error();
+	thread_local static char buf[256];
+	ERR_error_string_n(err, buf, sizeof(buf));
+	throw runtime_error(buf);
+}
--- cipher.h
+++ cipher.h
@@ -19,3 +19,14 @@ struct aes_128_cbc_cmac {
 	size_t encrypt(const key_type &key, const uint8_t *data, size_t len, uint8_t *encrypted, size_t cap);
 	size_t decrypt(const key_type &key, const uint8_t *data, size_t len, uint8_t *decrypted, size_t cap);
 };
+
+struct aes_128_gcm {
+	using key_type = ustring;
+
+	static const size_t iv_size = 16;
+	static const size_t tag_size = 16;
+	static const size_t min_cap = iv_size + tag_size + EVP_MAX_BLOCK_LENGTH;
+
+	size_t encrypt(const key_type &key, const uint8_t *data, size_t len, uint8_t *encrypted, size_t cap);
+	size_t decrypt(const key_type &key, const uint8_t *data, size_t len, uint8_t *decrypted, size_t cap);
+};
